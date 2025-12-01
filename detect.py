513b46f72004df492296fc7248ca3832ce9a7739
Natsu <natthasit08@gmail.com>
2025-12-01 12:40:03 +0700

    4th Mini fixed

--- detect.py
+++ detect.py
@@ -14,17 +14,21 @@ def init_model():
         return None
 
 def detect_process(frame, model):
-    detected_name = ""
+    detected_name = "" 
     
     results = model(frame, stream=True, verbose=False)
 
     for r in results:
         boxes = r.boxes
         for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
             conf = math.ceil((box.conf[0] * 100)) / 100
+
+            if conf < define.CONF_FILTER:
+                continue 
+
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
             cls = int(box.cls[0])
             current_name = model.names[cls] 
@@ -34,6 +38,5 @@ def detect_process(frame, model):
             cv2.rectangle(frame, (x1, y1), (x2, y2), define.BLUE, define.THICKNESS)
             cv2.putText(frame, f"{current_name} {conf}", (x1, y1 - 10), 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, define.BLUE, 2)
-
 
     return frame, detected_name
--- import_model/readme.txt
+++ import_model/readme.txt
@@ -1,1 +1,3 @@
 This Folder make to import model file "best.pt" or file ".pt" in this Folder
+
+Download Model here --> "https://drive.google.com/drive/folders/1_007pkj7mDFXCIbfLvnVRmnKkTUc7hxj?usp=drive_link"
